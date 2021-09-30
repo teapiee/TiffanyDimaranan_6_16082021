361fb3b919f2c68cbe7fd9a6272aa15f18ca90c1
Tiffany <tiffanydimaranan@gmail.com>
2021-09-30 09:55:51 +0200

    add verify sauce and like unlike dislike sauce

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -56,13 +56,21 @@ exports.deleteSauce = (req, res, next) => {
 
 exports.likeSauce = async (req, res, next) => {
 
+  // verify if sauce exists // 
+  const sauceFound = await Sauce.findOne({ _id: req.params.id }) 
+  console.log(sauceFound)
+  if (!sauceFound){
+    console.log("Sauce not found")
+    res.status(400).json({ error: "Sauce does not exist"})
+  }
+
   // verify if user exists // 
   const userId = req.body.userId; 
   if (!userId){
     res.status(400).json({ error: "A user id must be provided"})
   }
   if (!ObjectId.isValid(userId)){
-    res.status(400).json({ error: "A correct user id must be provided"})
+    res.status(403).json({ error: "Unauthorized request"})
   }
   const userFound = await User.findOne({_id: userId})
   console.log(userFound)
@@ -70,55 +78,88 @@ exports.likeSauce = async (req, res, next) => {
     res.status(404).json({ error: "User not found"})
   }
 
-  // verify if sauce exists // 
-  //const sauceFound = await Sauce.findOne({ _id: req.params.id }) 
-  //if (!sauceFound){
-    //res.status(400).json({ error: "Sauce not found"})
-  //}
-
   // verify if like value is correct // 
-  //const like = req.body.like; 
-    //if (like !== -1 && like !== 0 && like !== 1){
-      //return res.status(400).json({error: "Like value must equal to 1, 0 or -1"});
-    //}
+  const like = req.body.like; 
+    if (like !== -1 && like !== 0 && like !== 1){
+      return res.status(400).json({error: "Like value must equal to 1, 0 or -1"});
+    }
+  
+  // when sauce is liked //
+  if(like == 1){
+    Sauce.findOne({ _id: req.params.id })
+    .then(sauce =>{
+      if (sauce.usersLiked.includes(userFound._id)){
+        return res.status(403).json({error: "You already liked this sauce"})
+      }
+      sauce.likes++;
+      sauce.usersLiked.push(userFound._id);
+      console.log('User is trying to like the sauce')
 
-    /*if(like == 1){
-      Sauce.findOne({ _id: req.params.id })
-      .then(sauce =>{
-        sauce.like++;
-        sauce.usersLiked.push(sauce.userId);
-        console.log('User is trying to like the sauce')
-      })
-      .catch(error => res.status(404).json({ error}));
-    };
+      Sauce.updateOne({ _id: req.params.id },sauce )
+      .then(() => res.status(200).json({ message: "Like updated" })) 
+      .catch((error) => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(404).json({error}));
 
-    /*Sauce.updateOne({ _id: req.params.id },sauce )
-        .then(() => res.status(200).json({ message: "Like updated" })) 
-        .catch((error) => res.status(400).json({ error }));
+  }else 
 
-    }else if (like == 0){ 
+    // when sauce is unliked // 
+    if (like == 0){ 
       Sauce.findOne({ _id: req.params.id })
       .then(sauce => {
-          if (usersLiked.includes(userId) || usersDisliked.includes(userId)){
-          sauce.like--;
-          splice method to remove like from array
-            
-      Sauce.updateOne({ _id: req.params.id },sauce )
-      .then(() => res.status(200).json({ message: "Unlike updated" })) 
-      .catch((error) => res.status(400).json({ error }));
-      
-    } else if (like == -1){
-        Sauce.findOne({ _id: req.params.id })
-        .then(sauce => {
-          sauce.dislike--;
-          sauce.usersDisliked.push(sauce.userId);
-            
+
+        if (sauce.usersLiked.indexOf(userFound._id) == -1){
+          res.status(403).json({error: "Like this sauce first"})
+        } 
+
+        else{
+        sauce.likes--;
+        const index = sauce.usersLiked.indexOf(userFound._id); //splice method to remove like from array//
+          if (index > -1) {
+          sauce.usersLiked.splice(index, 1);
+          }
+        console.log('User is trying to unlike the sauce')
+        }
+
+
+        //if (sauce.usersDisliked.indexOf(userFound._id) == -1){
+        //  res.status(403).json({error: "Dislike this sauce first"})
+        //} 
+
+        //else{
+        //sauce.dislikes--;
+        //const index = sauce.usersDisliked.indexOf(userFound._id); //splice method to remove like from array//
+         // if (index > -1) {
+         // sauce.usersDisliked.splice(index, 1);
+         // }
+        //console.log('User is trying to unlike the sauce')
+        //}
+
+
         Sauce.updateOne({ _id: req.params.id },sauce )
-        .then(() => res.status(200).json({ message: "Dislike updated" })) 
+        .then(() => res.status(200).json({ message: "Unlike updated" })) 
         .catch((error) => res.status(400).json({ error }));
+      })
+      .catch(error => res.status(404).json({error}));
+    
+    }else
 
-    }else{}*/
-  /*.catch(error => res.status(500).json({ error }));  */
- 
+      // when sauce is disliked //
+      if (like == -1){
+        Sauce.findOne({ _id: req.params.id })
+        .then(sauce =>{
+          if (sauce.usersDisliked.includes(userFound._id)){
+            return res.status(403).json({error: "You already disliked this sauce"})
+          }
+          sauce.dislikes++;
+          sauce.usersDisliked.push(userFound._id);
+          console.log('User is trying to dislike the sauce')
+    
+          Sauce.updateOne({ _id: req.params.id },sauce )
+          .then(() => res.status(200).json({ message: "Dislike updated" })) 
+          .catch((error) => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(404).json({error}));
+      }
+
 };
-
