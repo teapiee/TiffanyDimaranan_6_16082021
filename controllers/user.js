0aeaa56803e355c1002ae9c71edbae66a696ea87
Tiffany <tiffanydimaranan@gmail.com>
2021-10-13 17:29:23 +0200

    add validator

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,13 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const validator = require('validator');
 
 
 exports.signup = (req, res, next) => {
+  if(!validator.isEmail(req.body.email) && !validator.isStrongPassword(req.body.password)){
+    res.status(400).json({error:"Email or password not valid"});
+  }
     bcrypt.hash(req.body.password, 10)
       .then(hash => {
         const user = new User({
@@ -11,7 +15,7 @@ exports.signup = (req, res, next) => {
           password: hash
         });
         user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+          .then(() => res.status(201).json({ message: 'User created!' }))
           .catch(error => res.status(400).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));
@@ -22,12 +26,12 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
       .then(user => {
         if (!user) {
-          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+          return res.status(401).json({ error: 'User not found!' });
         }
         bcrypt.compare(req.body.password, user.password)
           .then(valid => {
             if (!valid) {
-              return res.status(401).json({ error: 'Mot de passe incorrect !' });
+              return res.status(401).json({ error: 'Incorrect password!' });
             }
             res.status(200).json({
               userId: user._id,
